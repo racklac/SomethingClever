bd22d53bcdefdb48a8b276e8b579d5eadd10a20c
racklac <acrackl@my.chattanoogastate.edu>
2019-12-05 16:07:25 -0500

    I added js to play with

---
+++ somethingClever.js
@@ -0,0 +1,7 @@
+
+alert("connected");
+var h1 = document.querySelector("h1");
+var button = document.getElementById("button");
+button.addEventListener("click", function(){
+h1.textContent="WOo-hhooooo";
+});
