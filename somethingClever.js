40828c1e2410899a49ca4447e25914fc6e129a87
racklac <acrackl@my.chattanoogastate.edu>
2019-12-11 09:52:45 -0500

    test

--- somethingClever.js
+++ somethingClever.js
@@ -2,6 +2,12 @@
 alert("connected");
 var h1 = document.querySelector("h1");
 var button = document.getElementById("button");
+var trekButton = document.getElementById("trekButton");
+var imageSpock=document.querySelector("img");
 button.addEventListener("click", function(){
 h1.textContent="WOo-hhooooo";
+});
+trekButton.addEventListener("click", function() {
+    alert("i did a ghing");
+    imageSpock.style.width=20;
 });
